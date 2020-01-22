6c2a19c68e614610429dd90c6fd3c52b0d5e1475
Cade Barnett <cb346517@ohio.edu>
2020-01-22 18:50:23 -0500

    hello git program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Cade Barnett>
+ *        Date: <january 22, 2020>
+ * Description: lab2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
